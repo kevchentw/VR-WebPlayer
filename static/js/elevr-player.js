d17ae67b593821bed7c3af0d96ae864141c9b6bb
Kevin Chen <me@kevchentw.com>
2015-08-26 10:18:44 +0800

    add sound bar

--- static/js/elevr-player.js
+++ static/js/elevr-player.js
@@ -16,215 +16,217 @@
 
 var currentScreenOrientation = window.orientation || 0; // active default
 
-var timing = {showTiming: false, // Switch to true to show frame times in the console
-              frameTime: 0,
-              prevFrameTime: 0,
-              canvasResized: 0,
-              textureLoaded: 0,
-              textureTime: 0,
-              start: 0,
-              end: 0,
-              framesSinceIssue: 0
-              };
+var timing = {
+    showTiming: false, // Switch to true to show frame times in the console
+    frameTime: 0,
+    prevFrameTime: 0,
+    canvasResized: 0,
+    textureLoaded: 0,
+    textureTime: 0,
+    start: 0,
+    end: 0,
+    framesSinceIssue: 0
+};
 
 var called = {};
 var videoOptions = {};
 
 function resizeContainer() {
-  if (!window.container) {
-    window.container = document.getElementById('video-container');
-  }
+    if (!window.container) {
+        window.container = document.getElementById('video-container');
+    }
 
-  window.container.style.width = window.innerWidth + 'px';
-  window.container.style.height = window.innerHeight + 'px';
+    window.container.style.width = window.innerWidth + 'px';
+    window.container.style.height = window.innerHeight + 'px';
 }
 
 window.addEventListener('resize', resizeContainer);
 
 function setupControls() {
-  if (called.setupControls) {
-    return;
-  }
+    if (called.setupControls) {
+        return;
+    }
 
-  window.videoControls = document.getElementById('video-controls');
-  window.messageL = document.getElementById('message-l');
-  window.messageR = document.getElementById('message-r');
+    window.videoControls = document.getElementById('video-controls');
+    window.messageL = document.getElementById('message-l');
+    window.messageR = document.getElementById('message-r');
 
-  resizeContainer();
+    resizeContainer();
 
-  window.leftLoad = document.getElementById('left-load');
-  window.rightLoad = document.getElementById('right-load');
-  window.leftPlay = document.getElementById('left-play');
-  window.rightPlay = document.getElementById('right-play');
-  window.canvas = document.getElementById('glcanvas');
-  window.video = document.getElementById('video');
+    window.leftLoad = document.getElementById('left-load');
+    window.rightLoad = document.getElementById('right-load');
+    window.leftPlay = document.getElementById('left-play');
+    window.rightPlay = document.getElementById('right-play');
+    window.canvas = document.getElementById('glcanvas');
+    window.video = document.getElementById('video');
 
-  // Buttons
-  window.playButton = document.getElementById('play-pause');
-  window.playL = document.getElementById('play-l');
-  window.playR = document.getElementById('play-r');
-  window.muteButton = document.getElementById('mute');
-  window.loopButton = document.getElementById('loop');
-  window.fullScreenButton = document.getElementById('full-screen');
+    // Buttons
+    window.playButton = document.getElementById('play-pause');
+    window.playL = document.getElementById('play-l');
+    window.playR = document.getElementById('play-r');
+    window.volumeButton = document.getElementById('volume');
+    window.loopButton = document.getElementById('loop');
+    window.fullScreenButton = document.getElementById('full-screen');
 
-  // Sliders
-  window.seekBar = document.getElementById('seek-bar');
+    // Sliders
+    window.seekBar = document.getElementById('seek-bar');
+    window.volumeBar = document.getElementById('volume-bar');
 
-  // Selectors
-  window.videoSelect = document.getElementById('video-select');
-  window.projectionSelect = document.getElementById('projection-select');
+    // Selectors
+    window.videoSelect = document.getElementById('video-select');
+    window.projectionSelect = document.getElementById('projection-select');
 
-  document.getElementById('title-l').style.fontSize = window.outerHeight / 20 + 'px';
-  document.getElementById('title-r').style.fontSize = window.outerHeight / 20 + 'px';
+    document.getElementById('title-l').style.fontSize = window.outerHeight / 20 + 'px';
+    document.getElementById('title-r').style.fontSize = window.outerHeight / 20 + 'px';
 
-  window.messageL.style.fontSize = window.outerHeight / 30 + 'px';
-  window.messageR.style.fontSize = window.outerHeight / 30 + 'px';
+    window.messageL.style.fontSize = window.outerHeight / 30 + 'px';
+    window.messageR.style.fontSize = window.outerHeight / 30 + 'px';
 
-  controls.create();
+    controls.create();
 
-  called.setupControls = true;
+    called.setupControls = true;
 }
 
 function runEleVRPlayer() {
-  if (called.runEleVRPlayer) {
-    return;
-  }
+    if (called.runEleVRPlayer) {
+        return;
+    }
 
-  setupControls();
+    setupControls();
 
-  webVR.initWebVR();
+    webVR.initWebVR();
 
-  webGL.initWebGL();
+    webGL.initWebGL();
 
-  if (webGL.gl) {
-    webGL.gl.clearColor(0.0, 0.0, 0.0, 0.0);
-    webGL.gl.clearDepth(1.0);
-    webGL.gl.disable(webGL.gl.DEPTH_TEST);
+    if (webGL.gl) {
+        webGL.gl.clearColor(0.0, 0.0, 0.0, 0.0);
+        webGL.gl.clearDepth(1.0);
+        webGL.gl.disable(webGL.gl.DEPTH_TEST);
 
-    util.setCanvasSize();
+        util.setCanvasSize();
 
-    // Keyboard Controls
-    controls.enableKeyControls();
+        // Keyboard Controls
+        controls.enableKeyControls();
 
-    window.shader = new webGL.Shader({
-      fragmentShaderName: 'shader-fs',
-      vertexShaderName: 'shader-vs',
-      attributes: ['aVertexPosition'],
-      uniforms: ['uSampler', 'eye', 'projection', 'proj_inv'],
-    });
+        window.shader = new webGL.Shader({
+            fragmentShaderName: 'shader-fs',
+            vertexShaderName: 'shader-vs',
+            attributes: ['aVertexPosition'],
+            uniforms: ['uSampler', 'eye', 'projection', 'proj_inv'],
+        });
 
-    webGL.initBuffers();
-    webGL.initTextures();
+        webGL.initBuffers();
+        webGL.initTextures();
 
-    window.video.addEventListener('canplaythrough', controls.loaded);
-    window.video.addEventListener('ended', controls.ended);
+        window.video.addEventListener('canplaythrough', controls.loaded);
+        window.video.addEventListener('ended', controls.ended);
 
-    // Keep a record of all the videos that are in the drop-down menu.
-    Array.prototype.slice.call(window.videoSelect.options).forEach(function(option) {
-      videoOptions[option.value] = option;
-    });
-  }
+        // Keep a record of all the videos that are in the drop-down menu.
+        Array.prototype.slice.call(window.videoSelect.options).forEach(function (option) {
+            videoOptions[option.value] = option;
+        });
+    }
 
-  initFromSettings(window.location.hash || window.location.search);
+    initFromSettings(window.location.hash || window.location.search);
 
-  called.runEleVRPlayer = true;
+    called.runEleVRPlayer = true;
 }
 
 function initFromSettings(newSettings) {
-  if (!newSettings) {
-    controls.show();
-    return;
-  }
-
-  var settings = util.getTruthyURLSearchParams(newSettings, {
-    autoplay: undefined,
-    controls: true,
-    loop: true,
-    sound: true,
-    projection: 'mono'
-  });
-
-  if (settings.controls) {
-    controls.show();
-  } else {
-    controls.hide();
-
-    if (typeof settings.autoplay === 'undefined') {
-      // `autoplay` by default if controls are hidden and no explicit `autoplay` param set.
-      settings.autoplay = true;
+    if (!newSettings) {
+        controls.show();
+        return;
     }
-  }
 
-  if (settings.sound) {
-    controls.unmute();
-  } else {
-    controls.mute();
-  }
+    var settings = util.getTruthyURLSearchParams(newSettings, {
+        autoplay: undefined,
+        controls: true,
+        loop: true,
+        sound: true,
+        projection: 'mono'
+    });
 
-  settings.projection = util.getCustomProjection(settings.projection);
+    if (settings.controls) {
+        controls.show();
+    } else {
+        controls.hide();
 
-  if (projection !== settings.projection) {
-    projection = settings.projection;
-
-    if (window.projectionSelect) {
-      window.projectionSelect.value = settings.projection;
+        if (typeof settings.autoplay === 'undefined') {
+            // `autoplay` by default if controls are hidden and no explicit `autoplay` param set.
+            settings.autoplay = true;
+        }
     }
-  }
 
-  controls.setLooping(settings.loop);
+    if (settings.sound) {
+        controls.unmute();
+    } else {
+        controls.mute();
+    }
 
-  if (settings.video) {
-    window.video.innerHTML = '';
+    settings.projection = util.getCustomProjection(settings.projection);
 
-    if (window.videoSelect) {
-      var optionValue = settings.projection + settings.video;
+    if (projection !== settings.projection) {
+        projection = settings.projection;
 
-      if (optionValue in videoOptions) {
-        videoOptions[optionValue].selected = true;
-      } else {
-        var option = document.createElement('option');
-        option.selected = true;
-        option.textContent = settings.title || util.getVideoTitle(settings.video);
+        if (window.projectionSelect) {
+            window.projectionSelect.value = settings.projection;
+        }
+    }
 
-        // Note: The controls code expects the filename to be prefixed with '0' or '1'.
-        option.value = optionValue;
+    controls.setLooping(settings.loop);
 
-        if (settings.autoplay) {
-          option.dataset.autoplay = '';
-        } else {
-          delete option.dataset.autoplay;
+    if (settings.video) {
+        window.video.innerHTML = '';
+
+        if (window.videoSelect) {
+            var optionValue = settings.projection + settings.video;
+
+            if (optionValue in videoOptions) {
+                videoOptions[optionValue].selected = true;
+            } else {
+                var option = document.createElement('option');
+                option.selected = true;
+                option.textContent = settings.title || util.getVideoTitle(settings.video);
+
+                // Note: The controls code expects the filename to be prefixed with '0' or '1'.
+                option.value = optionValue;
+
+                if (settings.autoplay) {
+                    option.dataset.autoplay = '';
+                } else {
+                    delete option.dataset.autoplay;
+                }
+
+                videoOptions[optionValue] = option;
+
+                window.videoSelect.appendChild(option);
+            }
         }
 
-        videoOptions[optionValue] = option;
-
-        window.videoSelect.appendChild(option);
-      }
+        controls.loadVideo(settings.video);
     }
 
-    controls.loadVideo(settings.video);
-  }
-
-  if (settings.autoplay) {
-    controls.play();
-  } else if (settings.autoplay === false) {
-    // If user did not explicitly set `autoplay`, don't pause unnecessarily.
-    window.video.pause();
-  }
+    if (settings.autoplay) {
+        controls.play();
+    } else if (settings.autoplay === false) {
+        // If user did not explicitly set `autoplay`, don't pause unnecessarily.
+        window.video.pause();
+    }
 }
 
-window.addEventListener('hashchange', function() {
-  // Remove the querystring if there were custom parameters.
-  window.history.pushState('', document.title, window.location.pathname + window.location.hash);
+window.addEventListener('hashchange', function () {
+    // Remove the querystring if there were custom parameters.
+    window.history.pushState('', document.title, window.location.pathname + window.location.hash);
 
-  initFromSettings(window.location.hash);
+    initFromSettings(window.location.hash);
 });
 
-window.addEventListener('message', function(e) {
-  if (typeof e.data === 'object') {
-    window.location.hash = '#' + JSON.stringify(e.data);
-  } else if (typeof e.data === 'string') {
-    window.location.hash = '#' + e.data;
-  } else {
-    return;
-  }
+window.addEventListener('message', function (e) {
+    if (typeof e.data === 'object') {
+        window.location.hash = '#' + JSON.stringify(e.data);
+    } else if (typeof e.data === 'string') {
+        window.location.hash = '#' + e.data;
+    } else {
+        return;
+    }
 });
